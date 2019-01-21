1326f93095cc259553464dac5b51d3fc214beed3
Kokosoul <lostarkyam@gmail.com>
2019-01-20 20:54:33 -0500

    Added arkark logo and link to front page

--- frontend/src/components/navbar/topnavbar.jsx
+++ frontend/src/components/navbar/topnavbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Logo from '../../img/lostarklight.png';
+import "./topnavbar.css";
 import { Navbar, NavItem, Nav, NavbarBrand, NavLink } from 'reactstrap';
 
 class TopNavBar extends React.Component {
   render(){
       return(
         <Navbar color="light" light expand="md">
-          <NavbarBrand>Lost Ark Site</NavbarBrand>
+          <NavbarBrand href="/"><img src={Logo} alt="ArkArk Logo" className="Logo" /></NavbarBrand>
           <Nav className="ml-auto" navbar>
             <NavItem>
               <NavLink href='/admin'>Admin</NavLink>
